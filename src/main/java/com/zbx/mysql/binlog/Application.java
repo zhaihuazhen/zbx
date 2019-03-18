53c1389adcbd28c7857dc719d7b39f1de2022f73
zhaihuazhen <zhaihuazhen@zhihu.com>
2019-03-18 22:41:36 +0800

    init

---
+++ src/main/java/com/zbx/mysql/binlog/Application.java
@@ -0,0 +1,17 @@
+package com.zhihu.commercial.biz.bidding.billing;
+
+@SpringBootApplication
+public class Application {
+
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        logger.inf
+        try {
+            logger.
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+    }
+
+}
