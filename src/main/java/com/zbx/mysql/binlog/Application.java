c029d3344d3b74b7c35843d2e20202c90da4bb4f
zhaihuazhen <zhaihuazhen@zhihu.com>
2019-03-18 23:05:57 +0800

    test

--- src/main/java/com/zbx/mysql/binlog/Application.java
+++ src/main/java/com/zbx/mysql/binlog/Application.java
@@ -1,9 +1,34 @@
 package com.zbx.mysql.binlog;
 
 
+import com.github.shyiko.mysql.binlog.BinaryLogClient;
+import com.github.shyiko.mysql.binlog.event.Event;
+import com.github.shyiko.mysql.binlog.event.deserialization.EventDeserializer;
+
+import java.io.IOException;
+
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("测试服务系统");
+        try{
+            BinaryLogClient client = new BinaryLogClient("", 3306, "username", "password");
+            EventDeserializer eventDeserializer = new EventDeserializer();
+            eventDeserializer.setCompatibilityMode(
+                    EventDeserializer.CompatibilityMode.DATE_AND_TIME_AS_LONG,
+                    EventDeserializer.CompatibilityMode.CHAR_AND_BINARY_AS_BYTE_ARRAY
+            );
+            client.setEventDeserializer(eventDeserializer);
+            client.registerEventListener(new BinaryLogClient.EventListener() {
+
+                @Override
+                public void onEvent(Event event) {
+                }
+            });
+            client.connect();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
